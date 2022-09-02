602ae721b1dfffba6d03edaa8efb4818739fc1cd
Phan Quang Minh Long <lordaizensousuke2001@gmail.com>
2022-09-02 11:28:07 +0700

    first commit

---
+++ contactslist/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+
+from django.http import HttpResponse
+from .models import Contact
+
+def index(request):
+    return HttpResponse('Hello')
+
+def display(request):
+    contacts = Contact.objects.all()
+
+    s = ""
+    for c in contacts:
+        s += c.to_string() + '<br>'
+    return HttpResponse("Contacts:<br>" + s)
